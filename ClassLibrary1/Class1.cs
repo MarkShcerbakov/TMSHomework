f14f515ed83ab3af1fb32fd5d2cfa0aaa3374a0f
Mark Shcerbakov <markshcerbakov@gmail.com>
2024-12-19 20:48:10 +0300

    Add Lesson1-task1 branch

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace ClassLibrary1
+{
+    public class Person
+    {
+        public string Name { get; }
+        public string Birthdate { get; }
+        public Person(string name, string birthdate)
+        {
+            Name = name;
+            Birthdate = birthdate;
+        }
+    }
+}
--- TMSHomework/Program.cs
+++ TMSHomework/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace TMSHomework
+﻿using ClassLibrary1;
+
+namespace TMSHomework
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Homework after lesson 1 in TMS school.");
+
+            Console.WriteLine("Введите ваше ФИО:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите вашу дату рождения (дд.мм.гггг):");
+            string birthdate = Console.ReadLine();
+
+            Person person = new(name, birthdate);
+
+            Console.WriteLine($"Ваши данные: {person.Name}, дата рождения: {person.Birthdate}");
+            Console.ReadKey();
         }
     }
 }
