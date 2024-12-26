588750321b7d9b4d08a4b3156238a8582f9efbfe
Mark Shcerbakov <markshcerbakov@gmail.com>
2024-12-26 11:00:37 +0300

    Creat project

--- TMSHomework/Program.cs
+++ TMSHomework/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Описание приложения
+            Console.WriteLine("TMS Lesson4-Task3-MiniGame");
+            Console.WriteLine("Приложение моделируеи систему управления виртуальным игровым пространством.");
+            Console.WriteLine("В этом пространстве находятся клетки с разными объектами.");
+
         }
     }
 }
