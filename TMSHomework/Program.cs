87047f32a866f2a5e347d8b055ee6b5be9bbe7d8
Mark Shcerbakov <markshcerbakov@gmail.com>
2024-12-27 19:56:49 +0300

    Text input

--- TMSHomework/Program.cs
+++ TMSHomework/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("TMS Lesson5-Task1-WorkWithText");
+            Console.WriteLine("Приложение позволяет выводить информацию о введенном фрагменте текста.");
+            Console.WriteLine("В данном случае будет приведен отрывок из спортивной статьи.\n");
+
+            var inputText = "";
+            if (File.Exists("test.txt"))
+            {
+                inputText = File.ReadAllText("test.txt");
+            }
+            else
+            {
+                Console.WriteLine("Файл с текстом не найден!");
+            }
+            foreach (var item in inputText)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadKey();
+
         }
     }
 }
