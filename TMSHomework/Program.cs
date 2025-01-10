a9cc8bccb26433edf07b29e999728efd6098c98e
Mark Shcerbakov <markshcerbakov@gmail.com>
2025-01-11 02:01:01 +0300

    Task complete

--- TMSHomework/Program.cs
+++ TMSHomework/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("TMS Lesson8-Task1-Animal-Dog");
+            Console.WriteLine("Приложение осуществляет наследование класса Dog от абстрактного класса Aniumal.\n");
+
+            Console.WriteLine("Введите имя собаки:");
+            var dogName = Console.ReadLine();
+            var dog = new Dog();
+            dog.SetName(dogName);
+            Console.WriteLine($"Вы назвали собаку именем {dog.GetName()}");
+            Console.WriteLine("В настоящее время:");
+            dog.Eat();
+            Console.ReadKey();
         }
     }
 }
